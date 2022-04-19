c3ca463a67061ec1a85b28fb98d3eae42b6a5ef5
Sebakor <47070879@est.ort.edu.ar>
2022-04-19 15:19:27 -0300

    Initial commit

---
+++ Assets/Scripts/EJALTURA.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJALTURA : MonoBehaviour
+{
+
+    public int edad;
+    public float altura;
+    
+
+
+
+
+
+    void Start()
+    {
+        if (altura >= 1.2f && edad >= 13 ) 
+        {
+            Debug.Log(" Acceso permitodo ");
+
+        }
+        else
+        {
+            Debug.Log(" Acceso denegado " );
+        }
+
+        if (altura < 1f && edad < 1)
+        {
+            Debug.Log(" Error edad o altura no puede ser menor que 1 ");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
